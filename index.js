0edbeb35fc6a82a2a39b278a9aa2d28953591b39
mohamed200111 <mohakatalonya10@gmail.com>
2022-08-22 12:42:57 +0100

    Full functionality

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const slider = document.querySelector("#range-slider");
 const outputValue = document.querySelector(".output-value");
 const resetButton = document.querySelector(".reset");
 const blackBtn = document.querySelector(".black");
-const redBtn = document.querySelector(".red");
-const blueBtn = document.querySelector(".blue");
 const colorPicker = document.querySelector("#color-picker");
 const rgbBtn = document.querySelector(".rgb");
-let color = '';
-
+const gradBtn = document.querySelector(".grad");
+let color = "";
 
 // showing initial slider value in the paragraph
 outputValue.textContent = `${slider.value} x ${slider.value}`;
 
+// remove grid elements
 const removeGridElement = () => {
     const gridElements = document.querySelectorAll(".grid-element");
     gridElements.forEach((element) => {
         gridContainer.removeChild(element);
     });
-}
+};
+
 // showing Dynamic slider value in the paragraph
 // Creating grid element width relative to grid container
 // 510 is the width and height of the grid container
@@ -31,19 +31,15 @@ slider.addEventListener("input", () => {
 });
 
 // first executed when loading the page
-const createGrid = (width = '31.88') => {
+const createGrid = (width = "31.88") => {
     for (let i = 0; i < slider.value ** 2; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("grid-element");
         newDiv.style.width = `${width}px`;
         gridContainer.appendChild(newDiv);
-        newDiv.addEventListener("mouseover", () => {
-            newDiv.style.backgroundColor = color;
-        });
     }
-}
+};
 createGrid();
-
 
 // Reset grid elements background color
 const resetGridColor = () => {
@@ -51,26 +47,48 @@ const resetGridColor = () => {
     gridElements.forEach((element) => {
         element.style.backgroundColor = "inherit";
     });
-}
+};
 resetButton.addEventListener("click", resetGridColor);
 
+
+// buttons listener
+const addMouseEvent = (color) => {
+    const gridElements = document.querySelectorAll(".grid-element");
+    for (const element of gridElements) {
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = color;
+        });
+    }
+}
 blackBtn.addEventListener("click", () => {
     resetGridColor();
-    color = "hsl(0, 100%, 1%)";
+    addMouseEvent("black");
 });
-
-redBtn.addEventListener("click", () => {
-    resetGridColor();
-    color = "hsl(351, 100%, 50%)";
-});
-
-blueBtn.addEventListener("click", () => {
-    resetGridColor();
-    color = "hsl(241, 100%, 50%)";
-});
-
-
 colorPicker.addEventListener("click", () => {
     resetGridColor();
-    color = colorPicker.value;
+    addMouseEvent(colorPicker.value);
+});
+
+rgbBtn.addEventListener("click", () => {
+    resetGridColor();
+    const gridElements = document.querySelectorAll(".grid-element");
+    for (const element of gridElements) {
+        element.addEventListener("mouseover", () => {
+            const hue = Math.floor(Math.random() * 360);
+            element.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+        });
+    }
+});
+
+gradBtn.addEventListener("click", () => {
+    resetGridColor();
+    const gridElements = document.querySelectorAll(".grid-element");
+    let darker = 100;
+    for (const element of gridElements) {
+        element.addEventListener("mouseover", () => {
+            const hue = Math.floor(Math.random() * 360);
+            darker -= 2;
+            element.style.backgroundColor = `hsl(${hue}, 100%, ${darker}%)`;
+        });
+    }
 });
